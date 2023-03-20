3fea8a6ea6d82535116c322c35f1ce6d9014645b
ElKokk <left.kok@hotmail.com>
2023-03-20 14:40:47 +0100

    modified:   src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte

--- src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
@@ -1,7 +1,7 @@
-<div class="answer">My name is ...</div>
+<div class="answer">My name is <b>Eleftherios</b></div>
 
 <style>
   div.answer {
-    color: rgb(2, 96, 45);
+    color: rgb(226, 16, 16);
   }
 </style>
