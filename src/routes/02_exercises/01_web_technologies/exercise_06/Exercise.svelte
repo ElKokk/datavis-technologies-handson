4ac2d5eee3628ed090f234688cdd61fc33638de4
ElKokk <left.kok@hotmail.com>
2023-03-20 15:09:31 +0100

    modified:   src/routes/02_exercises/01_web_technologies/exercise_04/Exercise.svelte modified:   src/routes/02_exercises/01_web_technologies/exercise_05/Exercise.svelte modified:   src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte

--- src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
@@ -1,1 +1,8 @@
-I really like data visualisation!
+<style>
+p {
+    font-family: Times New Roman;
+    font-size: 20px;
+    color: steelblue;
+}
+</style>
+<p> I really like data visualization!</p>
