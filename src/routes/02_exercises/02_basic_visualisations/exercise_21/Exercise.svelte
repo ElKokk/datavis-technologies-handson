9edc79cda5fa0187890edc4d0e5eb2c4d4e52d20
ElKokk <left.kok@hotmail.com>
2023-04-24 00:07:58 +0300

    exercise 21

--- src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
@@ -1,24 +1,19 @@
 <script>
-  import { onMount } from 'svelte';
-  import Scatterplot from '/home/elkokk/datavis-technologies-handson/src/routes/02_exercises/02_basic_visualisations/exercise_21/Scatterplot.svelte';
+  import { onMount } from "svelte";
+  import Scatterplot from "/home/elkokk/datavis-technologies-handson/src/routes/02_exercises/02_basic_visualisations/exercise_21/Scatterplot.svelte";
 
   let data;
-  let countries1800;
+  let countriesFor1800;
 
   onMount(async () => {
-    try {
-      const response = await fetch('/home/elkokk/datavis-technologies-handson/static/data/gapminder.json');
-      data = await response.json();
-      console.log('Data:', data);
-      countries1800 = data.find(d => d.year === '1800').countries;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+    const response = await fetch("/home/elkokk/datavis-technologies-handson/static/data/gapminder.json");
+    data = await response.json();
+    countriesFor1800 = data.find((entry) => entry.year === 1800).countries;
   });
 </script>
 
-{#if !countries1800}
+{#if !countriesFor1800}
   <p>Loading data...</p>
 {:else}
-  <Scatterplot {countries1800} />
+  <Scatterplot {countriesFor1800} />
 {/if}
